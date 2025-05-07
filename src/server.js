198c323592b334f518097c1826f401eb791804b7
pravinpandeyit <pravinpandeyit575@gmail.com>
2025-05-07 19:00:58 +0530

    changes in server.js

--- src/server.js
+++ src/server.js
@@ -13,6 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req,res)=>{
+  res.send("Server is UP....")
+});
 app.use("/api/", userRoutes);
 app.use("/api/admin/", adminRoutes);
 
